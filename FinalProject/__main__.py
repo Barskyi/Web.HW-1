be69e5cf596812e79c191b58b4496a754322fbc5
Barskyi <137706923+Barskyi@users.noreply.github.com>
2023-10-29 20:25:35 +0200

    AbstractMain__main__.py

--- FinalProject/__main__.py
+++ FinalProject/__main__.py
@@ -1,24 +1,35 @@
 from Bot import Bot
+from abc import ABC
 
+class AbstractPrint(ABC):
+    def run(self):
+        pass
+
+class ContactAssistant(AbstractPrint):
+    def __init__(self):
+        self.bot = Bot()
+
+    def run(self):
+        print('Hello. I am your contact-assistant. What should I do with your contacts?')
+        self.bot.book.load("auto_save")
+        commands = ['Add', 'Search', 'Edit', 'Load', 'Remove', 'Save', 'Congratulate', 'View', 'Exit']
+        while True:
+            action = input('Type help for list of commands or enter your command\n').strip().lower()
+            if action == 'help':
+                format_str = str('{:%s%d}' % ('^', 20))
+                for command in commands:
+                    print(format_str.format(command))
+                action = input().strip().lower()
+                self.bot.handle(action)
+                if action in ['add', 'remove', 'edit']:
+                    self.bot.book.save("auto_save")
+            else:
+                self.bot.handle(action)
+                if action in ['add', 'remove', 'edit']:
+                    self.bot.book.save("auto_save")
+            if action == 'exit':
+                break
 
 if __name__ == "__main__":
-    print('Hello. I am your contact-assistant. What should I do with your contacts?')
-    bot = Bot()
-    bot.book.load("auto_save")
-    commands = ['Add', 'Search', 'Edit', 'Load', 'Remove', 'Save', 'Congratulate', 'View', 'Exit']
-    while True:
-        action = input('Type help for list of commands or enter your command\n').strip().lower()
-        if action == 'help':
-            format_str = str('{:%s%d}' % ('^',20))
-            for command in commands:
-                print(format_str.format(command))
-            action = input().strip().lower()
-            bot.handle(action)
-            if action in ['add', 'remove', 'edit']:
-                bot.book.save("auto_save")
-        else:
-            bot.handle(action)
-            if action in ['add', 'remove', 'edit']:
-                bot.book.save("auto_save")
-        if action == 'exit':
-            break
+    assistant = ContactAssistant()
+    assistant.run()
